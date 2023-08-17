12371be2ce5bcdb30a9ddc2c4457d20fcec97861
Islam Khabibullin <xab.is@yandex.ru>
2023-08-17 20:18:38 +0300

    Create generateSingleEliminationMatches

--- backend/src/main/java/ru/github/meperry/tms/backend/service/MatchService.java
+++ backend/src/main/java/ru/github/meperry/tms/backend/service/MatchService.java
@@ -1,7 +1,11 @@
 package ru.github.meperry.tms.backend.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -19,11 +23,11 @@ public class MatchService {
   private final MatchRepository matchRepository;
 
   public List<Match> generateRoundRobinMatches(List<User> participants) {
-    List<Match> matches = new ArrayList<>();
-
     if (participants == null || participants.size() < 2) {
       throw new IllegalArgumentException();
     }
+
+    List<Match> matches = new ArrayList<>();
 
     int numParticipants = participants.size();
 
@@ -48,7 +52,46 @@ public class MatchService {
   }
 
   public List<Match> generateSingleEliminationMatches(List<User> participants) {
+    if (participants == null || participants.size() < 2) {
+      throw new IllegalArgumentException();
+    }
 
+    int pairsCount = (int) Math.ceil(participants.size() / 2.0);
+    int matchCount = 1;
+    while (matchCount < pairsCount) {
+      matchCount *= 2;
+    }
+
+    int round = (int) (Math.log(matchCount) / Math.log(2));
+
+    List<Match> matches = Stream.generate(Match::new)
+        .limit(matchCount)
+        .collect(Collectors.toList());
+
+    Collections.shuffle(participants);
+
+    Iterator<User> participantIterator = participants.iterator();
+
+    // задаем первых игроков, а также раунд и порядковый номер матча
+    for (int i = 0; i < matchCount; i++) {
+      Match match = matches.get(i);
+      match.setRound(round);
+      match.setOrder(i);
+      match.setParticipantTwo(participantIterator.next());
+    }
+
+    // задаем вторых игроков
+    for (Match match : matches) {
+      // в какой-то момент участники могут закончится
+      if (participantIterator.hasNext()) {
+        match.setParticipantTwo(participantIterator.next());
+      }
+      else {
+        break;
+      }
+    }
+
+    return matches;
   }
 
   public List<Match> saveAll(List<Match> matches) {
